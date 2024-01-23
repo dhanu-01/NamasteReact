0ccc3a2d23dc432b772680ac6e3825e9e741c3c5
Dhanunjai K <dhanunjai@jmangroup.com>
2024-01-23 12:55:57 +0530

    igniting my app

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("h1", {id:'heading'}, "Hello world from React !");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const parent = React.createElement("div",
 {id:"parent"},
 [React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I'm an h1 tag"),
+[React.createElement("h1",{},"I'm an h1 tag from child 1"),
 React.createElement("h2",{}, "I'm an h2 tag"),
 ]
 ),
---
+++ README.md
@@ -0,0 +1,21 @@
+# Namaste REACT
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = HOT MODULE REPLACEMENT
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+
